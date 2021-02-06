88f5ce24b99dd968ac365d47f50e799d417bc45f
giovanni belval <giovanni.belval@icloud.com>
2021-02-06 15:59:22 +0000

    création des modèles V1
    -Utilisateur
    -Annonce
    -Contact
    -Question

--- Antigaspi/siteweb/admin.py
+++ Antigaspi/siteweb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Utilisateur)
+admin.site.register(models.Question)
+admin.site.register(models.Contact)
+admin.site.register(models.Annonce)
+
--- Antigaspi/siteweb/models.py
+++ Antigaspi/siteweb/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    statut = models.BooleanField(default = True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField( auto_now=True)
+
+    question = models.TextField(blank = False)
+    reponse = models.TextField(blank = False)
+
+class Contact(models.Model):
+    statut = models.BooleanField(default = True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField( auto_now=True)
+
+    localisation = models.TextField(blank = False)
+    email = models.EmailField( max_length=254)
+    telephone = models.TextField(blank = False)
+
+class Utilisateur(models.Model):
+    statut = models.BooleanField(default = True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField( auto_now=True)
+
+    nom = models.CharField( max_length=75)
+    prenom = models.CharField( max_length=75)
+    date_de_naissance = models.DateField()
+    email = models.EmailField( max_length=254)
+    localisation = models.TextField()
+    image = models.ImageField( upload_to='static/img')
+
+
+class Annonce(models.Model):
+    statut = models.BooleanField(default = True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_upd = models.DateTimeField( auto_now=True)
+
+    distributeur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    
+
+
